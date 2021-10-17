38551730c5086b71205a79722ed6f5b6a36ca943
richrd77@gmail.com <richrd77@gmail.com>
2021-10-17 17:38:58 +0530

    class 6

--- set2.js
+++ set2.js
@@ -19,7 +19,7 @@
 
 
 
-//THIS
+// THIS - conecpt
 // class Employee {
 //     constructor() {
 //         this.FirstName='';
@@ -69,14 +69,88 @@
 // DOM - Document Object Model
 // console.log(window)
 
-function ButtonClickEvent() {
-    console.log(document.getElementById('txt').value);
-    console.log(document.getElementById('dd').value);
-    console.log(document.getElementById('txta').value);
-}
+// function ButtonClickEvent() {
+//     console.log(document.getElementById('txt').value);
+//     console.log(document.getElementById('dd').value);
+//     console.log(document.getElementById('txta').value);
+// }
 
 
-let secondPElement = document.getElementById('h2');
-console.log(secondPElement.innerText);
+// let secondPElement = document.getElementById('h2');
+// console.log(secondPElement.innerText);
 
-console.log(document.getElementById('div').innerText)
+// console.log(document.getElementById('div').innerText)
+
+
+// event - any event [load, click, mouseover, mousein, mouseout, hover, unload]
+
+// function H1LoadEventHandler() {
+//     console.log('inside function');
+//     document.getElementById('h1').style.color = 'red';
+// }
+
+// function MouseINEventHandler() {
+//     console.log('mouse');
+//     document.getElementById('img').clientHeight = '1000px';
+// }
+
+
+// function FormSubmitEventHandler(a) {
+//     a.preventDefault();
+//     console.log(a);
+//     console.log('form Submitted');
+//     return false;
+// }
+
+// document.getElementById('myForm').addEventListener('submit', FormSubmitEventHandler);
+
+// document.getElementById('p').addEventListener('click', function ParentClickEvent() {
+//     console.log('Parent Click');
+// },);
+
+// document.getElementById('c').addEventListener('click', function () {
+//     console.log('Child Click')
+// });
+
+// document.getElementById('c2').addEventListener('click', function () {
+//     console.log('Child2 Click')
+// });
+
+// document.getElementById('gf').addEventListener('click', function () {
+//     console.log('Grand Father Click')
+// });
+
+
+// console.log('i')
+
+// let myFunction = function () {}
+
+// H1LoadEventHandler;
+
+// listeners
+
+// document.getElementById('myForm').addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     console.log(document.getElementById('name').value);
+//     console.log(document.getElementById('password').value);
+// });
+
+
+// function BtnClickEvent() {
+//     console.log(document.getElementById('u2').value);
+//     console.log(document.getElementById('p2').value);
+// }
+
+document.getElementById('btn').addEventListener('click', function () {
+    const dropDownValue = document.getElementById('dd1').value;
+    const txt = document.getElementById('txt');
+    const sp = document.getElementById('s');
+    if(dropDownValue == 'b') {
+        sp.style.backgroundColor = txt.value;
+    } else if(dropDownValue == 'c') {
+        sp.style.color = txt.value;
+    } else if(dropDownValue == 'v') {
+        sp.innerText = txt.value;
+    }
+    txt.value = '';
+});
