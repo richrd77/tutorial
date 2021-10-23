1b617bc72b376587b3f1ef0cc009448dfdcdfbc5
richrd77@gmail.com <richrd77@gmail.com>
2021-10-23 23:59:32 +0530

    class 11

--- form.js
+++ form.js
@@ -139,30 +139,115 @@
 // document.getElementById('btn').addEventListener('click', appendChidren);
 
 
-function addRow() {
-    //step1
-    const tableRow = document.createElement('tr');
-    const rowdiv1 = document.createElement('td');
-    const rowdiv2 = document.createElement('td');
-    const b = document.createElement('button');
+// function addRow() {
+//     //step1
+//     const tableRow = document.createElement('tr');
+//     const rowdiv1 = document.createElement('td');
+//     const rowdiv2 = document.createElement('td');
+//     const b = document.createElement('button');
 
-    //step2
-    rowdiv1.innerText = document.getElementById('txt').value;
+//     //step2
+//     rowdiv1.innerText = document.getElementById('txt').value;
 
-    b.innerText = 'delete';
-    b.onclick = function () {
-        const conf = confirm('are you sure you want to delete this row?');
-        if(conf == true) {
-            document.getElementById('tblbody').removeChild(tableRow);
-        }
-    };
-    rowdiv2.appendChild(b);
-    tableRow.appendChild(rowdiv1);
-    tableRow.appendChild(rowdiv2);
+//     b.innerText = 'delete';
+//     b.onclick = function () {
+//         const conf = confirm('are you sure you want to delete this row?');
+//         if(conf == true) {
+//             document.getElementById('tblbody').removeChild(tableRow);
+//         }
+//     };
+//     rowdiv2.appendChild(b);
+//     tableRow.appendChild(rowdiv1);
+//     tableRow.appendChild(rowdiv2);
 
 
-    //step3
-    document.getElementById('tblbody').appendChild(tableRow);
-}
+//     //step3
+//     document.getElementById('tblbody').appendChild(tableRow);
+// }
 
 // document.getElementById('btn').addEventListener('click', addRow);
+
+// cover call and apply
+
+// function add(...numbers) {
+//     // let normaladd = a + b;
+//                h.Display.call(e);
+//     // console.log([].reduce.call(arguments, function(x,y) {
+//     //     return Number(x) + Number(y);
+//     // },[]));
+
+//     return numbers.reduce((a, b) => a + b);
+
+//     // console.log(numbers);
+
+//     // console.log(arguments);
+
+// }
+
+// 38ab - arr1
+// arr2 - 1,2,3
+// let arr1 = [1,2,3];
+// let arr2 = [...arr1];
+
+// arr1.push(10);
+// arr1.push(20);
+
+// console.log(arr1);
+// console.log(arr2);
+
+
+// class Employee {
+//     constructor(n, age) {
+//         this.Name = n;
+//         this.age = age;
+//     }
+// }
+
+// class Human {
+//     constructor(n) {
+//         this.Name = n;
+//     }
+
+//     Display(message) {
+//         console.log(this);
+//         console.log(message);
+//     }
+// }
+
+
+// const h = new Human('Human Object');
+// const e = new Employee('My Employee', 25);
+
+// // h.Display();
+
+// h.Display.apply(e);
+// // console.log(e);
+'use strict'
+
+document.getElementById('btn').addEventListener('click', function () {
+    try {
+        //let num1 = Number(document.getElementById('n1').value);
+        //let num2 = Number(document.getElementById('n2').value);
+        //alert(num1 / num2);
+        
+        
+        // first api = []
+        //second api = []
+        let num1 = Number(document.getElementById('n1').value);
+        let num2 = Number(document.getElementById('n2').value);
+        if(num1 > num2) {
+            alert(num1 / num2);
+        } else {
+            throw 'num1 should be greater than num2';
+        }
+
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    } finally {
+        // delete []
+        // delete [] 
+        console.log('inside FInally');
+    }
+});
+
