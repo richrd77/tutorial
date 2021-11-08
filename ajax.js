62f8743e8240cb303f3b1dcdae37562fc918d46c
richrd77@gmail.com <richrd77@gmail.com>
2021-11-08 22:11:27 +0530

    class 15 HTTP GET

--- ajax.js
+++ ajax.js
@@ -31,29 +31,67 @@
 // const newEmployee2 = { Name:'Some Other Employee', Department:'IT' };
 
 //sample api response
-let apiResponse =
-    [
-        {
-            "userId": 1,
-            "id": 1,
-            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-        },
-        {
-            "userId": 1,
-            "id": 2,
-            "title": "qui est esse",
-            "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-        }
-    ];
-console.log(apiResponse.filter(o => o.id == 1));
-// console.log(JSON.stringify(apiResponse));
+// let apiResponse =
+//     [
+//         {
+//             "userId": 1,
+//             "id": 1,
+//             "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//             "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//         },
+//         {
+//             "userId": 1,
+//             "id": 2,
+//             "title": "qui est esse",
+//             "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+//         }
+//     ];
+// console.log(apiResponse.filter(o => o.id == 1));
+// // console.log(JSON.stringify(apiResponse));
 
 
-//json stringify & parse
-localStorage.setItem('res', JSON.stringify(apiResponse));
-let fromLocal = localStorage.getItem('res');
-let newarray = JSON.parse(fromLocal)
-console.log(newarray.filter(o => o.id == 1));
+// //json stringify & parse
+// localStorage.setItem('res', JSON.stringify(apiResponse));
+// let fromLocal = localStorage.getItem('res');
+// let newarray = JSON.parse(fromLocal)
+// console.log(newarray.filter(o => o.id == 1));
 
-    // localStorage.setItem('someString','My String Value');
+// localStorage.setItem('someString','My String Value');
+
+// ajax - asyncronus javascript and XML
+// HTTP request :- hyper Text Transfer protocol (plain Text)
+// HTTPs request :- hyper Text Transfer protocol secured (encrypted text - SSL certificate)
+// server -> client [get]
+// server <- client [Post]
+// server <- client [Put] with key (Id)
+// server <- client [delete] with key (Id)
+
+
+const request = new XMLHttpRequest();
+request.onreadystatechange = function (e, r) {
+    // console.log(r.responseText);
+    if (this.readyState == 4) {
+        // console.log(JSON.parse(request.responseText));
+        const respnse = JSON.parse(request.responseText);
+        respnse.forEach(function (o) {
+            addRow(o);
+        });
+    }
+    // console.log(e);
+}
+request.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+request.send();
+
+function addRow(obj) {
+    const newTr = document.createElement('tr');
+    const newTdTitle = document.createElement('td');
+    const newTdBody = document.createElement('td');
+    const newTdUserId = document.createElement('td');
+    newTdTitle.innerText = obj.title;
+    newTdBody.innerText = obj.body;
+    newTdUserId.innerText = obj.userId;
+    newTr.appendChild(newTdTitle);
+    newTr.appendChild(newTdBody);
+    newTr.appendChild(newTdUserId);
+    document.getElementById('tbl').appendChild(newTr);
+}
